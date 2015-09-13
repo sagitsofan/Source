cabaa9fe55cf7ef4c090a9156318ec6da13f605f
Stav Mizrahi <stavmaz@gmail.com>
2015-09-13 15:34:42 +0300

    app

---
+++ App/www/start.bat
@@ -0,0 +1,15 @@
+﻿//start client
+cd C:\Users\USER\Desktop\Sgol\Project\ITApp\Node.App
+ionic serve
+
+//start api
+cd C:\Users\USER\Desktop\Sgol\Project\ITApp\Node.Api
+node server
+
+//start mongo ui tool
+cd C:\Users\USER\Desktop\Sgol\Project\ITApp\Node.MongoUI\node_modules\mongo-express
+node app
+
+//start mongo
+cd C:\Program Files\MongoDB\Server\3.0\bin
+mongod
