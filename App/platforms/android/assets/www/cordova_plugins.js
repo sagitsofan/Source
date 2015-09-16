d1d824ddca45eb9a5751cd066d139c0be6d4bf8c
Stav Mizrahi <stavmaz@gmail.com>
2015-09-16 13:29:40 +0300

    facebook fixes

--- App/platforms/android/assets/www/cordova_plugins.js
+++ App/platforms/android/assets/www/cordova_plugins.js
@@ -7,12 +7,18 @@ module.exports = [
             "cordova.InAppBrowser.open",
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-inappbrowser": "1.0.1"
+    "cordova-plugin-inappbrowser": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
